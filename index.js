1aeb2eee0809160d18c6e73ea3559dd4a884aef0
niv7488 <niv7488@gmail.com>
2016-06-22 21:10:46 +0300

    cors

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var url = require('url');
 
 var port = process.env.PORT || 3000;
 
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With, Content-Type,Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function(req,res) {
